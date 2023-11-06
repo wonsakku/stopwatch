546d1513b0de4bf0d03d1a88675b3b0189d3156e
wonsakku <kws1044@gmail.com>
2023-11-06 20:54:08 +0900

    project init

---
+++ src/main/java/com/toyproject/stopwatch/StudyStopwatchApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.stopwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyStopwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyStopwatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyproject/stopwatch/StudyStopwatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.stopwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyStopwatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
